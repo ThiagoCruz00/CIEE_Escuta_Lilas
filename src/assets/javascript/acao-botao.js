e3b88f445644d1339e90c7099076d6a57326bb7f
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2025-08-18 21:45:01 -0300

    feat: adicionando botão para o 180

--- src/assets/javascript/acao-botao.js
+++ src/assets/javascript/acao-botao.js
@@ -7,17 +7,35 @@ botao.addEventListener("click", () => {
 const botao190 = document.querySelector(".btn.alerta");
 
 botao190.addEventListener("click", (e) => {
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
 
   if (isMobile) {
     window.location.href = "tel:190";
   } else {
-
     e.preventDefault();
     alert(
       "A função de ligar diretamente para 190 só está disponível em celulares. Por favor, use um telefone para entrar em contato."
+    );
+  }
+});
+
+const botao180 = document.querySelector(".btn.ligue180");
+
+botao180.addEventListener("click", (e) => {
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isMobile) {
+    window.location.href = "tel:180";
+  } else {
+    e.preventDefault();
+    alert(
+      "A função de ligar diretamente para 180 só está disponível em celulares. Para mais informações ou registrar a denúncia, acesse o site oficial."
     );
   }
 });
